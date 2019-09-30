c6f0ffa4536641074f8f5be5788ef6fea6992b69
Ted Ogmundson <togmund@gmail.com>
2019-09-30 02:15:01 +0000

    Use conditionals to tell people how to dress based on the weather

---
+++ conditionals.js
@@ -0,0 +1,18 @@
+const raining = false;
+const temperature = 22;
+
+if (raining) {
+  console.log("Don't forget your umbrella!");
+} else {
+  console.log("Don't forget your hat!")
+}
+
+if (temperature < 0) {
+  console.log("Make sure you pick out a scarf!")
+} else if (temperature < 15){
+  console.log("Short sleeves won't cut it!")
+} else {
+  console.log("Short sleeves are fine.")
+}
+
+console.log("Now you're ready to go outside!")
