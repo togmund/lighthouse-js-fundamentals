6616ce2fde2e7bb172d5b8d6118c22ffa1d9dc73
Ted Ogmundson <togmund@gmail.com>
2019-09-30 02:19:36 +0000

    add more conditional, hilarious, examples

--- conditionals.js
+++ conditionals.js
@@ -3,11 +3,13 @@ const temperature = 22;
 
 if (raining) {
   console.log("Don't forget your umbrella!");
-} else {
+} else if (!raining) {
   console.log("Don't forget your hat!")
 }
 
-if (temperature < 0) {
+if (temperature < -60 || temperature > 60) {
+  console.log("Are you out of your mind!? You can't go outside!")
+} else if (temperature < 0) {
   console.log("Make sure you pick out a scarf!")
 } else if (temperature < 15){
   console.log("Short sleeves won't cut it!")
