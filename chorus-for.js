22153913b070543bb63a33e6f9fb279f8fdcb6ba
Ted Ogmundson <togmund@gmail.com>
2019-09-30 05:59:48 +0000

    use a for loop to sing bowie

---
+++ chorus-for.js
@@ -0,0 +1,9 @@
+const chorus = "Let's dance!";
+const shoes = "Put on your red shoes and dance the blues";
+for (let repeat = 0; repeat < 3; repeat++) {
+  for (let repeat = 0; repeat < 2; repeat++) {
+    console.log(chorus);
+  }
+  console.log(shoes);
+}
+console.log("Under the moonlight, this serious moonlight");
