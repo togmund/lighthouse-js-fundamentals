71682e416ad1ad20ecbf6bdfb4fbac7d18c97373
Ted Ogmundson <togmund@gmail.com>
2019-10-01 16:37:22 +0000

    change the loop type to a 'while' and popped out utensils to push in cast iron

--- packing-list.js
+++ packing-list.js
@@ -1,8 +1,13 @@
 const packingList = ["bowls", "plates", "pots", "pans", "eating utensils", "glasses", "cups", "cooking utensils",];
 
+// Who needs cooking utensils when you have eating utensils?
+packingList.pop();
+// What, am I going to forget my cast iron?
 packingList.push("cast iron");
 
 console.log("Kitchen stuff to pack:");
-for (let i = 0; i < packingList.length; i++) {
+let i = 0;
+while (i < packingList.length) {
   console.log(packingList[i]);
+  i++;
 }
