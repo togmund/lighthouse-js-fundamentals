2913ce16f6e6a61b602a4cabb07a3b47c5b348aa
Ted Ogmundson <togmund@gmail.com>
2019-10-01 17:00:43 +0000

    added for loop to ingredients.js. also added some newlines and loop explainer text for readability in node

--- ingredients.js
+++ ingredients.js
@@ -1,12 +1,20 @@
 const ingredients = ["eggs", "milk", "flour", "sugar", "baking soda", "baking powder", "chocolate chips", "bananas"];
 
 // Write a while loop that prints out the contents of ingredients:
+console.log("\n");
+console.log("While loop: ");
 let i = 0;
 while (i < ingredients.length) {
   console.log(ingredients[i]);
   i++;
 }
+console.log("\n");
 
 // Write a for loop that prints out the contents of ingredients:
+console.log("For loop: ");
+for (let i = 0; i < ingredients.length; i++) {
+  console.log(ingredients[i]);
+}
+console.log("\n");
 
 // Write any loop (while or for) that prints out the contents of ingredients backwards:
